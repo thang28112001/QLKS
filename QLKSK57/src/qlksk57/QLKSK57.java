853c65478cc4a0986d42fbe5648387300a19b79a
thang28112001 <thangtien28112001@gmail.com>
2022-04-22 23:27:23 +0700

    first commit

---
+++ QLKSK57/src/qlksk57/QLKSK57.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qlksk57;
+
+import static qlksk57.MyConnection.getConnection;
+
+/**
+ *
+ * @author khoa
+ */
+public class QLKSK57 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        getConnection();
+        
+        
+    }
+    
+}
